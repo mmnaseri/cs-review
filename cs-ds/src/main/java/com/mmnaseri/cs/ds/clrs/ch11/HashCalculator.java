82c76187fc7c4cb199a8c50fb3a3efe01c93d727
mmnaseri <mmnaseri@programmer.net>
2015-07-13 15:37:47 -0700

    feat (clrs.ch11.s2): implement and test hash table with chaining resolution of collision

---
+++ cs-ds/src/main/java/com/mmnaseri/cs/ds/clrs/ch11/HashCalculator.java
@@ -0,0 +1,11 @@
+package com.mmnaseri.cs.ds.clrs.ch11;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (7/13/15)
+ */
+public interface HashCalculator {
+
+    int calculate(int index, int capacity);
+
+}
---
+++ cs-ds/src/main/java/com/mmnaseri/cs/ds/clrs/ch11/s3/ModularHashCalculator.java
@@ -0,0 +1,16 @@
+package com.mmnaseri.cs.ds.clrs.ch11.s3;
+
+import com.mmnaseri.cs.ds.clrs.ch11.HashCalculator;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (7/13/15)
+ */
+public class ModularHashCalculator implements HashCalculator {
+
+    @Override
+    public int calculate(int index, int capacity) {
+        return index % capacity;
+    }
+
+}

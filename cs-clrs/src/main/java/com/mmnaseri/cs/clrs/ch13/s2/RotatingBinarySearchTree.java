e3cb1839ace181bed2e15170adce92e44509286d
Mohammad Milad Naseri <m.m.naseri@gmail.com>
2015-07-19 23:50:05 -0700

    feat (clrs.ch13.s2): implement and test rotation for a binary tree

---
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch13/s2/RotatingBinarySearchTree.java
@@ -0,0 +1,61 @@
+package com.mmnaseri.cs.clrs.ch13.s2;
+
+import com.mmnaseri.cs.clrs.ch10.s4.impl.BinaryTreeNode;
+import com.mmnaseri.cs.clrs.ch12.s2.TreeNodeFactory;
+import com.mmnaseri.cs.clrs.ch12.s3.BinarySearchTree;
+
+import java.util.Comparator;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (7/19/15, 10:57 PM)
+ */
+public class RotatingBinarySearchTree<E, N extends BinaryTreeNode<E>> extends BinarySearchTree<E, N> {
+
+    public RotatingBinarySearchTree(Comparator<E> comparator, TreeNodeFactory<E, N> factory) {
+        super(comparator, factory);
+    }
+
+    protected void rotateRight(N target) {
+        //noinspection unchecked
+        final N substitute = (N) target.getLeftChild();
+        if (substitute == null) {
+            throw new IllegalStateException();
+        }
+        if (target.getParent() != null) {
+            if (target.getParent().getRightChild() == target) {
+                target.getParent().setRightChild(substitute);
+            } else {
+                target.getParent().setLeftChild(substitute);
+            }
+        } else {
+            setRoot(substitute);
+        }
+        target.setLeftChild(substitute.getRightChild());
+        substitute.setParent(target.getParent());
+        substitute.setRightChild(target);
+        target.setParent(substitute);
+    }
+
+    protected void rotateLeft(N target) {
+        //noinspection unchecked
+        final N substitute = (N) target.getRightChild();
+        if (substitute == null) {
+            throw new IllegalStateException();
+        }
+        if (target.getParent() != null) {
+            if (target.getParent().getRightChild() == target) {
+                target.getParent().setRightChild(substitute);
+            } else {
+                target.getParent().setLeftChild(substitute);
+            }
+        } else {
+            setRoot(substitute);
+        }
+        target.setRightChild(substitute.getLeftChild());
+        substitute.setParent(target.getParent());
+        substitute.setLeftChild(target);
+        target.setParent(substitute);
+    }
+
+}
---
+++ cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch13/s2/RotatingBinarySearchTreeTest.java
@@ -0,0 +1,74 @@
+package com.mmnaseri.cs.clrs.ch13.s2;
+
+import com.mmnaseri.cs.clrs.ch10.s4.impl.BinaryTreeNode;
+import com.mmnaseri.cs.clrs.ch12.s2.TreeNodeFactory;
+import org.testng.annotations.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.IsNot.not;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (7/19/15, 11:06 PM)
+ */
+public class RotatingBinarySearchTreeTest {
+
+    protected RotatingBinarySearchTree<Integer, BinaryTreeNode<Integer>> tree() {
+        return new RotatingBinarySearchTree<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        }, new TreeNodeFactory<Integer, BinaryTreeNode<Integer>>() {
+            @Override
+            public BinaryTreeNode<Integer> createNode(Integer value) {
+                final BinaryTreeNode<Integer> node = new BinaryTreeNode<>();
+                node.setValue(value);
+                return node;
+            }
+        });
+    }
+
+    @Test
+    public void testRotateLeft() throws Exception {
+        final RotatingBinarySearchTree<Integer, BinaryTreeNode<Integer>> tree = tree();
+        tree.insert(5, 3, 7, 1, 4, 6, 9, 8, 10);
+        final BinaryTreeNode<Integer> node = tree.find(7);
+        tree.rotateLeft(node);
+        assertThat(node.getParent(), is(notNullValue()));
+        assertThat(node.getParent().getValue(), is(9));
+        assertThat(node.getLeftChild(), is(notNullValue()));
+        assertThat(node.getLeftChild().getValue(), is(6));
+        assertThat(node.getRightChild(), is(notNullValue()));
+        assertThat(node.getRightChild().getValue(), is(8));
+    }
+
+    @Test
+    public void testRotateRight() throws Exception {
+        final RotatingBinarySearchTree<Integer, BinaryTreeNode<Integer>> tree = tree();
+        tree.insert(5, 3, 9, 1, 4, 7, 10, 6, 8);
+        final BinaryTreeNode<Integer> node = tree.find(9);
+        tree.rotateRight(node);
+        assertThat(node.getParent(), is(notNullValue()));
+        assertThat(node.getParent().getValue(), is(7));
+        assertThat(node.getLeftChild(), is(notNullValue()));
+        assertThat(node.getLeftChild().getValue(), is(8));
+        assertThat(node.getRightChild(), is(notNullValue()));
+        assertThat(node.getRightChild().getValue(), is(10));
+    }
+
+    @Test
+    public void testSymmetry() throws Exception {
+        final RotatingBinarySearchTree<Integer, BinaryTreeNode<Integer>> tree = tree();
+        tree.insert(5, 3, 7, 1, 4, 6, 8);
+        final String original = tree.toString();
+        tree.rotateLeft(tree.getRoot());
+        assertThat(tree.toString(), is(not(original)));
+        tree.rotateRight(tree.getRoot());
+        assertThat(tree.toString(), is(original));
+    }
+}

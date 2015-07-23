efac96ec09a672488bd74f19b38d8ff7de709c52
mmnaseri <mmnaseri@programmer.net>
2015-07-23 15:50:39 -0700

    fix (clrs.ch16.s1): fix brute force algorithm implementation

--- cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch16/s1/BruteForceActivitySelector.java
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch16/s1/BruteForceActivitySelector.java
@@ -1,6 +1,7 @@
 package com.mmnaseri.cs.clrs.ch16.s1;
 
 import com.mmnaseri.cs.clrs.ch15.s4.SequenceUtils;
+import com.mmnaseri.cs.qa.Complexity;
 import com.mmnaseri.cs.qa.Quality;
 import com.mmnaseri.cs.qa.Stage;
 
@@ -10,25 +11,26 @@ import java.util.*;
  * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
  * @since 1.0 (7/22/15)
  */
-@Quality(value = Stage.BUGGY, explanation = "poorest design and running time possible (O(2^n))")
+@Quality(value = Stage.DOCUMENTED, explanation = "poorest design and running time possible (O(2^n * n^2))")
 public class BruteForceActivitySelector implements ActivitySelector {
 
     @Override
+    @Complexity(value = "O(n^2 * 2^n)", explanation = "`findProperCandidate` is the most significant operation")
     public Set<Integer> select(Activity... activities) {
-        final List<List<IndexedActivity>> sequences = SequenceUtils.subSequences(Arrays.asList(IndexedActivity.index(activities)));
-        Collections.sort(sequences, new Comparator<List<IndexedActivity>>() {
-            @Override
-            public int compare(List<IndexedActivity> first, List<IndexedActivity> second) {
-                final int comparison = Integer.compare(second.size(), first.size());
-                if (comparison == 0) {
-                    return Integer.compare(sum(second), sum(first));
-                }
-                return comparison;
-            }
-        });
-        for (List<IndexedActivity> sequence : sequences) {
-            Collections.sort(sequence);
-        }
+        //we first note the index of the activities so that after sorting we can recall where each item was
+        final IndexedActivity[] indexedActivities = IndexedActivity.index(activities);
+        //now we calculate all the possible subsequences of the given list
+        final List<List<IndexedActivity>> sequences = generateSequences(indexedActivities);
+        //next we sort these sub-sequences so that the most profitable candidate is evaluated first
+        sortAllSubSequences(sequences);
+        //we now sort each sub-sequence so that items appear with their natural order
+        sortIndividualSubSequence(sequences);
+        //now we progressively process each subset to find the first one that matches our specification
+        return findProperCandidate(sequences);
+    }
+
+    @Complexity(value = "O(n^2 * 2^n)", explanation = "There are 2^n subsequences and each one is validated using O(n^2) compatibility control")
+    private Set<Integer> findProperCandidate(List<List<IndexedActivity>> sequences) {
         final Set<Integer> indices = new HashSet<>();
         for (List<IndexedActivity> sequence : sequences) {
             if (isCompatible(sequence)) {
@@ -41,6 +43,34 @@ public class BruteForceActivitySelector implements ActivitySelector {
         return indices;
     }
 
+    @Complexity(value = "O(2^n * nlgn)", explanation = "Each subsequence is of size order O(n) and merge sorting it requires O(nlgn) and there are 2^n such lists")
+    private void sortIndividualSubSequence(List<List<IndexedActivity>> sequences) {
+        for (List<IndexedActivity> sequence : sequences) {
+            Collections.sort(sequence);
+        }
+    }
+
+    @Complexity(value = "O(n2^n)", explanation = "We are sorting a collection of size 2^n using merge sort which is O(2^nlg(2^n)) = O(n2^n)")
+    private void sortAllSubSequences(List<List<IndexedActivity>> sequences) {
+        Collections.sort(sequences, new Comparator<List<IndexedActivity>>() {
+            @Override
+            public int compare(List<IndexedActivity> first, List<IndexedActivity> second) {
+                final int comparison = Integer.compare(second.size(), first.size());
+                if (comparison == 0) {
+                    //our algorithm should go for the sequence that finishes sooner (which leaves the most amount of unused space)
+                    return Integer.compare(first.get(first.size() - 1).getFinish(), second.get(second.size() - 1).getFinish());
+                }
+                return comparison;
+            }
+        });
+    }
+
+    @Complexity(value = "O(2^n)", explanation = "There are 2^n possible subsets for each set of size n, and we are generating them all dynamically")
+    private List<List<IndexedActivity>> generateSequences(IndexedActivity[] indexedActivities) {
+        return SequenceUtils.subSequences(Arrays.asList(indexedActivities));
+    }
+
+    @Complexity(value = "O(n^2)", explanation = "Each two activities are compared once: O(selection 2 of n) = O(n^2)")
     private static boolean isCompatible(List<? extends Activity> activities) {
         for (int i = 0; i < activities.size(); i++) {
             for (int j = 0; j < i; j++) {
@@ -50,14 +80,6 @@ public class BruteForceActivitySelector implements ActivitySelector {
             }
         }
         return true;
-    }
-
-    private static int sum(List<? extends Activity> activities) {
-        int sum = 0;
-        for (Activity activity : activities) {
-            sum += activity.getFinish() - activity.getStart();
-        }
-        return sum;
     }
 
 }
--- cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch16/s1/BruteForceActivitySelectorTest.java
+++ cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch16/s1/BruteForceActivitySelectorTest.java
@@ -4,9 +4,9 @@ package com.mmnaseri.cs.clrs.ch16.s1;
  * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
  * @since 1.0 (7/22/15)
  */
-public class BruteForceActivitySelectorTest {//extends BaseActivitySelectorTest {
+public class BruteForceActivitySelectorTest extends BaseActivitySelectorTest {
 
-//    @Override
+    @Override
     protected ActivitySelector getActivitySelector() {
         return new BruteForceActivitySelector();
     }

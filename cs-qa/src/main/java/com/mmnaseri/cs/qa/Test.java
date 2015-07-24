e93ae9bb833b0606f2d0c18466eec7002652bd50
mmnaseri <mmnaseri@programmer.net>
2015-07-24 12:39:45 -0700

    feat (qa): add test related annotations

---
+++ cs-qa/src/main/java/com/mmnaseri/cs/qa/Test.java
@@ -0,0 +1,20 @@
+package com.mmnaseri.cs.qa;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (7/24/15)
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Test {
+
+    String name();
+
+    String method() default "";
+
+}

f489a082d914d83c8d8190cfe980a88a0f851909
Mohammad Milad Naseri <m.m.naseri@gmail.com>
2015-07-25 14:13:55 -0700

    chore (all): optimize imports

--- cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch12/s3/SimpleBinarySearchTreeTest.java
+++ cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch12/s3/SimpleBinarySearchTreeTest.java
@@ -6,9 +6,7 @@ import org.testng.annotations.Test;
 import java.util.Comparator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 
 /**
  * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
--- cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch15/s4/BruteForceLongestCommonSubSequenceFinderTest.java
+++ cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch15/s4/BruteForceLongestCommonSubSequenceFinderTest.java
@@ -1,14 +1,5 @@
 package com.mmnaseri.cs.clrs.ch15.s4;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
 /**
  * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
  * @since 1.0 (7/20/15)

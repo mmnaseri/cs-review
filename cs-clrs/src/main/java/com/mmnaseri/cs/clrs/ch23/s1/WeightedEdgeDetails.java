c13f512f5aa3a9b60dee2d9ec20278dfe6ff0583
mmnaseri <mmnaseri@programmer.net>
2015-08-05 12:06:18 -0700

    feat (clrs.ch23.s1): add weighted edge details

---
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch23/s1/WeightedEdgeDetails.java
@@ -0,0 +1,21 @@
+package com.mmnaseri.cs.clrs.ch23.s1;
+
+import com.mmnaseri.cs.clrs.ch22.s1.EdgeDetails;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (8/5/15)
+ */
+public class WeightedEdgeDetails extends EdgeDetails {
+
+    private int weight;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}

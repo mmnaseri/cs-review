171ab78a8378ff5ec8465db607d03943265162b0
mmnaseri <mmnaseri@programmer.net>
2015-08-13 15:55:39 -0700

    feat (clrs.ch2.s1): implement faster binary sorts

---
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch02/s1/FasterBinaryInsertionSorter.java
@@ -0,0 +1,25 @@
+package com.mmnaseri.cs.clrs.ch02.s1;
+
+import java.util.Comparator;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (8/13/15)
+ */
+public class FasterBinaryInsertionSorter<E extends Comparable<E>> extends BinaryInsertionSorter<E> {
+
+    public FasterBinaryInsertionSorter(Comparator<E> comparator) {
+        super(comparator);
+    }
+
+    @Override
+    protected int findPlacement(E[] items, E item, int length, Comparator<E> comparator) {
+        if (comparator.compare(items[0], item) > 0) {
+            return 0;
+        } else if (comparator.compare(items[length - 1], item) < 0) {
+            return length;
+        }
+        return super.findPlacement(items, item, length, comparator);
+    }
+
+}
---
+++ cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch02/s1/FasterBinaryInsertionSorterTest.java
@@ -0,0 +1,17 @@
+package com.mmnaseri.cs.clrs.ch02.s1;
+
+import com.mmnaseri.cs.clrs.common.BaseSortTest;
+import com.mmnaseri.cs.clrs.common.Sorter;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (8/13/15)
+ */
+public class FasterBinaryInsertionSorterTest extends BaseSortTest {
+
+    @Override
+    protected Sorter<Integer> getAscendingSorter() {
+        return new FasterBinaryInsertionSorter<>(NATURAL_COMPARATOR);
+    }
+
+}

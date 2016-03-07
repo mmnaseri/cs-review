d531113e83ee575ea3d8c29e0010b421624b1d28
Naseri, Milad <milad.naseri@cdk.com>
2016-03-07 09:43:38 -0800

    feat(clrs.ch27.s2): implement simple matrix multiplication

---
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch27/s2/ScheduledSimpleMatrixMultiplier.java
@@ -0,0 +1,61 @@
+package com.mmnaseri.cs.clrs.ch27.s2;
+
+import com.mmnaseri.cs.clrs.ch04.s2.MatrixMultiplier;
+import com.mmnaseri.cs.clrs.ch27.s0.LoopStep;
+import com.mmnaseri.cs.clrs.ch27.s0.Scheduler;
+import com.mmnaseri.cs.clrs.ch27.s0.SchedulerFactory;
+import com.mmnaseri.cs.clrs.common.Matrix;
+import com.mmnaseri.cs.clrs.common.MatrixFactory;
+import com.mmnaseri.cs.clrs.common.NumberUtils;
+import com.mmnaseri.cs.clrs.common.impl.ArrayMatrixFactory;
+import com.mmnaseri.cs.qa.annotation.Quality;
+import com.mmnaseri.cs.qa.annotation.Stage;
+
+/**
+ * @author Milad Naseri (milad.naseri@cdk.com)
+ * @since 1.0 (2016/03/07, 08:55)
+ */
+@Quality(Stage.UNTESTED)
+public class ScheduledSimpleMatrixMultiplier implements MatrixMultiplier {
+
+    private final MatrixFactory factory;
+    private final Scheduler scheduler;
+
+    public ScheduledSimpleMatrixMultiplier(SchedulerFactory schedulerFactory) {
+        this(schedulerFactory, new ArrayMatrixFactory());
+    }
+
+    public ScheduledSimpleMatrixMultiplier(SchedulerFactory schedulerFactory, MatrixFactory factory) {
+        this.scheduler = schedulerFactory.getScheduler();
+        this.factory = factory;
+    }
+
+    @Override
+    public <E extends Number> Matrix<E> multiply(final Matrix<E> first, final Matrix<E> second) {
+        if (first.getRows() != second.getColumns()) {
+            throw new IllegalArgumentException("Incompatible matrix dimensions");
+        }
+        final Matrix<E> result = factory.getMatrix(second.getRows(), first.getColumns());
+        scheduler.loop(0, first.getColumns(), new LoopStep() {
+            @Override
+            public void perform(final int j) {
+                scheduler.loop(0, second.getRows(), new LoopStep() {
+                    @SuppressWarnings("Duplicates")
+                    @Override
+                    public void perform(final int i) {
+                        E value = null;
+                        for (int k = 0; k < first.getRows(); k ++) {
+                            final E firstValue = first.get(k, j);
+                            final E secondValue = second.get(i, k);
+                            final E multiplication = NumberUtils.multiply(firstValue, secondValue);
+                            value = NumberUtils.add(value, multiplication);
+                        }
+                        result.set(i, j, value);
+                    }
+                });
+            }
+        });
+        return result;
+    }
+
+}
---
+++ cs-clrs/src/test/java/com/mmnaseri/cs/clrs/ch27/s2/ScheduledSimpleMatrixMultiplierTest.java
@@ -0,0 +1,95 @@
+package com.mmnaseri.cs.clrs.ch27.s2;
+
+import com.mmnaseri.cs.clrs.ch04.s2.MatrixMultiplier;
+import com.mmnaseri.cs.clrs.ch27.s0.SchedulerFactory;
+import com.mmnaseri.cs.clrs.ch27.s0.impl.ParallelSchedulerFactory;
+import com.mmnaseri.cs.clrs.ch27.s0.impl.SerialSchedulerFactory;
+import com.mmnaseri.cs.clrs.common.Matrix;
+import com.mmnaseri.cs.clrs.common.impl.ArrayMatrix;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Milad Naseri (milad.naseri@cdk.com)
+ * @since 1.0 (2016/03/07, 09:01)
+ */
+public class ScheduledSimpleMatrixMultiplierTest {
+
+    @DataProvider
+    public Object[][] schedulerFactoryProvider() {
+        return new Object[][]{
+            new Object[]{new ParallelSchedulerFactory()},
+            new Object[]{new SerialSchedulerFactory()}
+        };
+    }
+
+    /**
+     *
+     * |  2  3  |
+     * |  0  4  |  * |  6  1  1  | =  |  11   27 |
+     * | -1  5  |    |  0  3  2  |    |  -2   22 |
+     *
+     */
+    @Test(dataProvider = "schedulerFactoryProvider")
+    public void testMultiplication(SchedulerFactory schedulerFactory) throws Exception {
+        final Matrix<Integer> first = new ArrayMatrix<>(3, 2);
+        first.set(0, 0, 2);
+        first.set(0, 1, 3);
+        first.set(1, 0, 0);
+        first.set(1, 1, 4);
+        first.set(2, 0, -1);
+        first.set(2, 1, 5);
+        final Matrix<Integer> second = new ArrayMatrix<>(2, 3);
+        second.set(0, 0, 6);
+        second.set(0, 1, 1);
+        second.set(0, 2, 1);
+        second.set(1, 0, 0);
+        second.set(1, 1, 3);
+        second.set(1, 2, 2);
+        final MatrixMultiplier multiplier = new ScheduledSimpleMatrixMultiplier(schedulerFactory);
+        final Matrix<Integer> multiplication = multiplier.multiply(first, second);
+        assertThat(multiplication.getColumns(), is(2));
+        assertThat(multiplication.getRows(), is(2));
+        assertThat(multiplication.get(0, 0), is(11));
+        assertThat(multiplication.get(0, 1), is(27));
+        assertThat(multiplication.get(1, 0), is(-2));
+        assertThat(multiplication.get(1, 1), is(22));
+    }
+
+    @Test
+    public void testIntegrity() {
+        final Matrix<Integer> first = new ArrayMatrix<>(3, 2);
+        first.set(0, 0, 2);
+        first.set(0, 1, 3);
+        first.set(1, 0, 0);
+        first.set(1, 1, 4);
+        first.set(2, 0, -1);
+        first.set(2, 1, 5);
+        final Matrix<Integer> second = new ArrayMatrix<>(2, 3);
+        second.set(0, 0, 6);
+        second.set(0, 1, 1);
+        second.set(0, 2, 1);
+        second.set(1, 0, 0);
+        second.set(1, 1, 3);
+        second.set(1, 2, 2);
+        final Matrix<Integer> firstMultiplication = new ScheduledSimpleMatrixMultiplier(new SerialSchedulerFactory()).multiply(first, second);
+        final Matrix<Integer> secondMultiplication = new ScheduledSimpleMatrixMultiplier(new ParallelSchedulerFactory()).multiply(first, second);
+        for (int i = 0; i < firstMultiplication.getRows(); i++) {
+            for (int j = 0; j < firstMultiplication.getColumns(); j++) {
+                assertThat(firstMultiplication.get(i, j), is(secondMultiplication.get(i, j)));
+            }
+        }
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Incompatible matrix dimensions")
+    public void testIncompatibleMatrices() throws Exception {
+        final Matrix<Integer> first = new ArrayMatrix<>(3, 3);
+        final Matrix<Integer> second = new ArrayMatrix<>(4, 4);
+        final MatrixMultiplier multiplier = new ScheduledSimpleMatrixMultiplier(new SerialSchedulerFactory());
+        multiplier.multiply(first, second);
+    }
+
+}

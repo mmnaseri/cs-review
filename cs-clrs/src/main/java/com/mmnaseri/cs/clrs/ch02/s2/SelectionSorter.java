09f99fb488bb7cbc7370e2236acea445a775bb4b
Milad Naseri <mmnaseri@programmer.net>
2016-06-26 21:25:24 -0700

    docs(clrs.ch02.s2): add documentation and complexity analysis to all classes and methods

--- cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch02/s2/SelectionSorter.java
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch02/s2/SelectionSorter.java
@@ -2,6 +2,7 @@ package com.mmnaseri.cs.clrs.ch02.s2;
 
 import com.mmnaseri.cs.clrs.common.ArrayUtils;
 import com.mmnaseri.cs.clrs.common.Sorter;
+import com.mmnaseri.cs.qa.annotation.Complexity;
 import com.mmnaseri.cs.qa.annotation.Quality;
 import com.mmnaseri.cs.qa.annotation.Stage;
 
@@ -11,7 +12,7 @@ import java.util.Comparator;
  * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
  * @since 1.0 (5/26/15, 2:49 AM)
  */
-@Quality(Stage.TESTED)
+@Quality(Stage.DOCUMENTED)
 public class SelectionSorter<E extends Comparable<E>> implements Sorter<E> {
 
     private final Comparator<E> comparator;
@@ -20,7 +21,13 @@ public class SelectionSorter<E extends Comparable<E>> implements Sorter<E> {
         this.comparator = comparator;
     }
 
+    /**
+     * This method implements the "selection" sort algorithm by considering the shrinking tail of the array (which contains
+     * the full array at the start) and then finding the local minimum and placing it at the beginning of the array.
+     * @param items    the items to be sorted
+     */
     @Override
+    @Complexity("O(n^2)")
     public void sort(E[] items) {
         for (int i = 0; i < items.length; i ++) {
             int localMinimum = -1;

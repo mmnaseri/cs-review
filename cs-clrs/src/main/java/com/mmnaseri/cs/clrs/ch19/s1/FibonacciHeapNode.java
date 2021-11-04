6efe2d81a64751a40b9a717977f9eafb1c682bd7
Milad Naseri <m.m.naseri@gmail.com>
2021-11-03 17:43:44 -0700

    chapter 19: code cleanup

--- cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch19/s1/FibonacciHeapNode.java
+++ cs-clrs/src/main/java/com/mmnaseri/cs/clrs/ch19/s1/FibonacciHeapNode.java
@@ -6,79 +6,78 @@ package com.mmnaseri.cs.clrs.ch19.s1;
  */
 public class FibonacciHeapNode<E extends Comparable<E>> {
 
-    private E value;
-    private boolean marked;
-    private FibonacciHeapNode<E> previous;
-    private FibonacciHeapNode<E> next;
-    private FibonacciHeapNode<E> child;
-    private FibonacciHeapNode<E> parent;
-    private int degree;
+  private E value;
+  private boolean marked;
+  private FibonacciHeapNode<E> previous;
+  private FibonacciHeapNode<E> next;
+  private FibonacciHeapNode<E> child;
+  private FibonacciHeapNode<E> parent;
+  private int degree;
 
-    public FibonacciHeapNode(E value) {
-        this.value = value;
-        this.marked = false;
-        this.degree = 0;
-    }
+  public FibonacciHeapNode(E value) {
+    this.value = value;
+    this.marked = false;
+    this.degree = 0;
+  }
 
-    public E getValue() {
-        return value;
-    }
+  public E getValue() {
+    return value;
+  }
 
-    public void setValue(E value) {
-        this.value = value;
-    }
+  public void setValue(E value) {
+    this.value = value;
+  }
 
-    public boolean isMarked() {
-        return marked;
-    }
+  public boolean isMarked() {
+    return marked;
+  }
 
-    public void setMarked(boolean marked) {
-        this.marked = marked;
-    }
+  public void setMarked(boolean marked) {
+    this.marked = marked;
+  }
 
-    public int getDegree() {
-        return degree;
-    }
+  public int getDegree() {
+    return degree;
+  }
 
-    public void setDegree(int degree) {
-        this.degree = degree;
-    }
+  public void setDegree(int degree) {
+    this.degree = degree;
+  }
 
-    public FibonacciHeapNode<E> getPrevious() {
-        return previous;
-    }
+  public FibonacciHeapNode<E> getPrevious() {
+    return previous;
+  }
 
-    public void setPrevious(FibonacciHeapNode<E> previous) {
-        this.previous = previous;
-    }
+  public void setPrevious(FibonacciHeapNode<E> previous) {
+    this.previous = previous;
+  }
 
-    public FibonacciHeapNode<E> getNext() {
-        return next;
-    }
+  public FibonacciHeapNode<E> getNext() {
+    return next;
+  }
 
-    public void setNext(FibonacciHeapNode<E> next) {
-        this.next = next;
-    }
+  public void setNext(FibonacciHeapNode<E> next) {
+    this.next = next;
+  }
 
-    public FibonacciHeapNode<E> getChild() {
-        return child;
-    }
+  public FibonacciHeapNode<E> getChild() {
+    return child;
+  }
 
-    public void setChild(FibonacciHeapNode<E> child) {
-        this.child = child;
-    }
+  public void setChild(FibonacciHeapNode<E> child) {
+    this.child = child;
+  }
 
-    public FibonacciHeapNode<E> getParent() {
-        return parent;
-    }
+  public FibonacciHeapNode<E> getParent() {
+    return parent;
+  }
 
-    public void setParent(FibonacciHeapNode<E> parent) {
-        this.parent = parent;
-    }
+  public void setParent(FibonacciHeapNode<E> parent) {
+    this.parent = parent;
+  }
 
-    @Override
-    public String toString() {
-        return String.valueOf(getValue());
-    }
-
+  @Override
+  public String toString() {
+    return String.valueOf(getValue());
+  }
 }
